33f51f7a8bbdb03d7041883636ddda1506b332e4
185510765@qq.com <185510765@qq.com>
2018-05-09 10:02:21 +0800

    PHP文件流下载完成

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,8 +1,82 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
 class IndexController extends Controller {
-    public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+
+	// // 构造函数
+	// public function __construct(){
+	// 	parent::__construct();
+	// 	if (!checkLogin())
+	// 	{
+	// 		$this->redirect('/Home/Index/login');
+	// 	}
+	// }
+
+	//下载页面
+    public function download(){
+        $this->display('download');
     }
+
+    //点击下载
+    public function clickDownload(){
+		$User=M('User');
+		$map['admin']=array('EQ',transformData(I('get.username')));     //get传参  真正登录后就有数据了
+		$info=$User->field('UserID,username,wbCount')->where($map)->find();
+		if ($info) {  
+			//这里写下载的判断条件，以后会修改
+			if ($info['wbCount']>C('WBCOUNT')) {    //网吧数量大于10才给下载
+
+				//文件流下载函数
+				function download($fname,$fpath='Public/download/'){
+				    //避免中文文件名出现检测不到文件名的情况，进行转码utf-8->gbk
+				    $filename=iconv('utf-8', 'gb2312', $fname);
+				    $path=$fpath.$filename;
+				    if(!file_exists($path)){//检测文件是否存在
+				        echo "文件不存在！";
+				        die();
+				    }
+				    $fp=fopen($path,'r');//只读方式打开
+				    $filesize=filesize($path);//文件大小
+
+				    //返回的文件(流形式)
+				    header("Content-type: application/octet-stream");
+				    //按照字节大小返回
+				    header("Accept-Ranges: bytes");
+				    //返回文件大小
+				    header("Accept-Length: $filesize");
+				    //这里客户端的弹出对话框，对应的文件名
+				    header("Content-Disposition: attachment; filename=".$filename);
+				    
+				    //================重点====================
+				    ob_clean();
+				    flush();
+				    //=================重点===================
+				    //设置分流
+				    $buffer=1024;
+				    //来个文件字节计数器
+				    $count=0;
+				    while(!feof($fp)&&($filesize-$count>0)){
+				        $data=fread($fp,$buffer);
+				        $count+=$data;//计数
+				        echo $data;//传数据给浏览器端
+				    }
+				    fclose($fp);
+				}
+				download('腾讯.exe');
+			}else{
+				// echo '很抱歉，网吧数量大于10家的用户才可以下载';
+				$this->redirect('Home/User/login','',3,'很抱歉，网吧数量大于10家的用户才可以下载,跳转登录页面中...');
+			}
+		}else{
+			// echo '用户不存在';
+			$this->redirect('Home/User/login','',3,'用户不存在,跳转登录页面中...');
+		}
+    }
+
+
+
+
+
+
 }
---
+++ Public/Home/js/download.js
@@ -0,0 +1,28 @@
+$(function(){
+	//点击下载
+	$('.download').click(function(){
+		// var UserID=$('.UserID').val();
+		// if (UserID=='' || UserID==null) {
+		// 	$.messager.alert('提示信息','您还没有登录，请先登录','info',function(){
+		// 		window.location.href=APP+'/Home/User/login';
+		// 	});
+		// 	return false;
+		// }
+
+		// window.location.href=CONTROLLER+'/clickDownload/UserID/'+UserID;
+		window.location.href=CONTROLLER+'/clickDownload';
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+});
